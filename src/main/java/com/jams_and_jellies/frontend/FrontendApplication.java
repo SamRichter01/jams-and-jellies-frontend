e35545acf7233c5472e9022643f22bd1d9290f1b
RICH9794 <rich9794@stthomas.edu>
2025-03-29 16:54:34 -0500

    initial commit

---
+++ src/main/java/com/jams_and_jellies/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.jams_and_jellies.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+}
